146895e74953c688c9948ccc767bf20cc612f9ae
José Pedro do Amaral <zpamaral@gmail.com>
2025-07-24 10:28:31 +0100

    ...

--- FlacCoverQL.h
+++ FlacCoverQL.h
@@ -1,5 +1,8 @@
+#include <CoreFoundation/CoreFoundation.h>
 #import <QuickLook/QuickLook.h>
+#ifdef __OBJC__
 #import <Foundation/Foundation.h>
+#endif
 
 OSStatus GeneratePreviewForURL(void *thisInterface,
                                QLPreviewRequestRef preview,
